83575ea941f0dc0b31ddf5371f788bfcc8d9f20a
Thore Krüss <thore@kruess.xyz>
2017-06-25 13:33:51 +0200

    Linter fixes

--- src/calculator.js
+++ src/calculator.js
@@ -1,25 +1,21 @@
 var calc = new function() {
 	this.add = function(a, b) {
-		return a+b;
+		return a + b;
 	};
-	
-	this.subtract = function (a, b) {
-		return a-b;
+	this.subtract = function(a, b) {
+		return a - b;
 	};
-
-	this.multiply = function (a, b) {
-		return a*b;
+	this.multiply = function(a, b) {
+		return a * b;
 	};
-	
-	this.divide = function (a, b) {
-		return a/b;
+	this.divide = function(a, b) {
+		return a / b;
 	};
-
-	this.pow = function (a, b) {
+	this.pow = function(a, b) {
 		var result = a;
-		for (var i = 1; i<b; i++) {
+		for (var i = 1; i < b; i++) {
 			result *= a;
 		}
 		return result;
 	};
-}
+};
